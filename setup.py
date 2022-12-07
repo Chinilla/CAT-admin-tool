a620819c93ed9f5e03fced2dfa302066550c2bf4
Edward Teach <edward@edwardteach.co>
2022-12-06 21:48:19 -0800

    updated version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ dev_dependencies = [
 
 setup(
     name="chinilla_CAT_admin",
-    version="1.0.0",
+    version="1.1.0",
     author="Edward Teach",
     entry_points={
         "console_scripts": [
